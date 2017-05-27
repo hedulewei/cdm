f88f029a0cf677f8f43bfdb812d4c074e2074849
yangchenglin <2849978760@qq.com>
2017-05-27 17:43:44 +0800

    some

---
+++ CDMservers/CDMservers/Controllers/BiologicalRecognitionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CDMservers.Models;
+
+namespace CDMservers.Controllers
+{
+    public class BiologicalRecognitionController : ApiController
+    {
+        private UserDbc db = new UserDbc();
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+     
+    }
+}
---
+++ Common/UserRole.cs
@@ -0,0 +1,4 @@
+﻿namespace Common
+{
+    public enum UserRole { Audit, Accept, Pay, Certificate }
+}
