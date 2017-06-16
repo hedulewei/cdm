f3e567448c0f2a22654400d2d9fccd82636de00d
yangchenglin <2849978760@qq.com>
2017-06-16 18:08:01 +0800

    minor

--- SyncViaFiles/src/SyncViaFiles/Program.cs
+++ SyncViaFiles/src/SyncViaFiles/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,19 @@ namespace SyncViaFiles
         public static void Main(string[] args)
         {
             Console.WriteLine("haha");
-         //   Process.Start("sh /home/fyang/release/syncftp.sh"); Console.WriteLine("hellow world");
+            //   Process.Start("sh /home/fyang/release/syncftp.sh"); Console.WriteLine("hellow world");
+            DirectoryInfo a = new DirectoryInfo("/home/fyang/release");
+            foreach(var b in a.GetFiles())
+            {
+                //if (b.Name == "")
+                //{
+                   var content= File.ReadAllLines(b.FullName);
+                    foreach(var line in content)
+                    {
+                    Console.WriteLine(b.Name+line);
+                }
+               // }
+            }
             Console.ReadLine();
         }
     }
--- syncdata/Program.cs
+++ syncdata/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace syncdata
 {
@@ -6,7 +8,36 @@ namespace syncdata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("haha");
+            //   Process.Start("sh /home/fyang/release/syncftp.sh"); Console.WriteLine("hellow world");
+
+
+            Console.ReadLine();
+        }
+
+        void bbb()
+        {
+            using (var db = new studyContext())
+            {
+                DirectoryInfo a = new DirectoryInfo("/home/fyang/release");
+                foreach (var b in a.GetFiles())
+                {
+                    //if (b.Name == "")
+                    //{
+                    var content = File.ReadAllLines(b.FullName);
+                    foreach (var line in content)
+                    {
+                        Console.WriteLine(b.Name + line);
+                        var theuser = db.User.FirstOrDefault(async => async.Identity == "identity");
+                        if (theuser == null)
+                        {
+                            ;
+                        }
+                    }
+                    // }
+                }
+            }
         }
     }
+
 }
