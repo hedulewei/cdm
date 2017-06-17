f85345006fdb1cc63d35401ac9c821a46bb5a927
yangchenglin <2849978760@qq.com>
2017-06-17 11:40:03 +0800

    intranet

--- study/Controllers/InspectController.cs
+++ study/Controllers/InspectController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 namespace study.Controllers
 {
-        public class InspectController : Controller
+    public class InspectController : Controller
     {
 
         private readonly studyContext _db1 = new studyContext();
@@ -17,7 +17,7 @@ namespace study.Controllers
             public string Identity { get; set; }
             public string Token { get; set; }
         }
-       
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -28,10 +28,10 @@ namespace study.Controllers
             base.Dispose(disposing);
         }
 
-    
+
         [Route("InspectCompleteCourses")]
         [HttpPost]
-       
+
         public CommonResponse InspectCompleteCourses([FromBody] CompleteCoursesRequest inputRequest)
         {
             try
@@ -62,29 +62,30 @@ namespace study.Controllers
                 }
                 theuser.Studycompletedate = DateTime.Now;
                 theuser.Studylog += inputRequest.AllRecords + inputRequest.AllStatus;
-               
-                _db1.History.Add(new History{
-                    Identity=theuser.Identity,
-                    Name=theuser.Name,
-                    Phone=theuser.Phone,
-                    Syncdate=theuser.Syncdate,
-                    Studystartdate=theuser.Studystartdate,
 
-                    Studycompletedate=theuser.Studycompletedate,
-                    Stoplicense=theuser.Stoplicense,
-                    Noticedate=theuser.Noticedate,
-                    Wechat=theuser.Wechat,
-                    Studylog=theuser.Studylog,
+                _db1.History.Add(new History
+                {
+                    Identity = theuser.Identity,
+                    Name = theuser.Name,
+                    Phone = theuser.Phone,
+                    Syncdate = theuser.Syncdate,
+                    Studystartdate = theuser.Studystartdate,
 
-                    Drugrelated=theuser.Drugrelated,
-                    Photo=theuser.Photo,
-                    Fullmark=theuser.Fullmark,
-                    Inspect=theuser.Inspect,
-                    Licensetype=theuser.Licensetype,
-                    Timestamp=DateTime.Now
+                    Studycompletedate = theuser.Studycompletedate,
+                    Stoplicense = theuser.Stoplicense,
+                    Noticedate = theuser.Noticedate,
+                    Wechat = theuser.Wechat,
+                    Studylog = theuser.Studylog,
+
+                    Drugrelated = theuser.Drugrelated,
+                    Photo = theuser.Photo,
+                    Fullmark = theuser.Fullmark,
+                    Inspect = theuser.Inspect,
+                    Licensetype = theuser.Licensetype,
+                    Timestamp = DateTime.Now
                 });
                 _db1.User.Remove(theuser);
-                 _db1.SaveChanges();
+                _db1.SaveChanges();
                 return new CommonResponse
                 {
                     StatusCode = "100000",
@@ -101,6 +102,7 @@ namespace study.Controllers
         }
         [Route("InspectPostStudyStatus")]
         [HttpPost]
+
         public CommonResponse InspectPostStudyStatus([FromBody] StudyStatusRequest inputRequest)
         {
             try
@@ -109,8 +111,34 @@ namespace study.Controllers
                 {
                     return new CommonResponse { StatusCode = "100002", Description = "请求错误，请检查请求参数！" };
                 }
-                //   Log.Info("PostStudyStatus input:" + JsonConvert.SerializeObject(inputRequest));
+                var found = false;
+                var identity = string.Empty;
+                foreach (var a in tokens)
+                {
+                    if (a.Token == inputRequest.Token)
+                    {
+                        identity = a.Identity;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    return new CommonResponse { StatusCode = "100001", Description = "error token" };
+                }
+                var theuser = _db1.User.FirstOrDefault(async => async.Identity == identity);
+                if (theuser == null)
+                {
+                    return new GetLearnerInfoResponse { StatusCode = "100004", Description = "error identity" };
+                }
 
+                if (theuser.Studystartdate == null)
+                {
+                    theuser.Studystartdate = DateTime.Now;
+                }
+                Console.WriteLine("startdate,{0}", theuser.Studystartdate);
+                theuser.Studylog += JsonConvert.SerializeObject(inputRequest);
+                _db1.SaveChanges();
                 return new CommonResponse
                 {
                     StatusCode = "100000",
@@ -126,6 +154,7 @@ namespace study.Controllers
         }
         [Route("InspectGetLearnerInfo")]
         [HttpGet]
+
         public GetLearnerInfoResponse InspectGetLearnerInfo(string token)
         {
             try
@@ -134,15 +163,35 @@ namespace study.Controllers
                 {
                     return new GetLearnerInfoResponse { StatusCode = "100001", Description = "error token" };
                 }
-                var toke1n = GetToken();
+                var found = false;
+                var identity = string.Empty;
+                foreach (var a in tokens)
+                {
+                    if (a.Token == token)
+                    {
+                        identity = a.Identity;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    return new GetLearnerInfoResponse { StatusCode = "100001", Description = "error token" };
+                }
+                var theuser = _db1.User.FirstOrDefault(async => async.Identity == identity);
+                if (theuser == null)
+                {
+                    return new GetLearnerInfoResponse { StatusCode = "100004", Description = "error identity" };
+                }
                 return new GetLearnerInfoResponse
                 {
                     StatusCode = "100000",
                     Description = "ok",
-                    DrivingLicenseType = DrivingLicenseType.A,
-                    Identity = "id",
-                    Name = "name",
-                    ///  Photo = new byte[1]
+                    DrivingLicenseType = string.IsNullOrEmpty(theuser.Licensetype) ? DrivingLicenseType.Unknown : (DrivingLicenseType)int.Parse(theuser.Licensetype),
+
+                    Identity = theuser.Identity,
+                    Name = theuser.Name,
+                    //  Photo = new byte[1]
                 };
             }
             catch (Exception ex)
@@ -152,15 +201,10 @@ namespace study.Controllers
             }
 
         }
-
         private string GetToken()
         {
             var seed = Guid.NewGuid().ToString("N");
             return seed;
         }
-
-
-
-
     }
 }
