3e23bb0998b80b821ea2c26edd0ebeed2b00dec2
yangchenglin <2849978760@qq.com>
2017-06-18 08:26:48 +0800

    delete

--- CDMservers/Webodp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Webodp.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
